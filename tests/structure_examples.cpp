d6fa92005f9d17549999150c70e7c5f10595cdf7
euna <christinejung10@daum.net>
2023-11-21 18:25:53 +0900

    feat: tests - structure examples

--- tests/structure_examples.cpp
+++ tests/structure_examples.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 
 TEST(TestSuiteName1, TestName1) {
-    cout << "Yaho" << endl;
+    cout << "Yaho1" << endl;
 }
 
 TEST(TestSuiteName1, TestName2) {
     EXPECT_EQ(0, 0);
+    cout << "Yaho2" << endl;
     EXPECT_NE(0, 1);
 }
 
 TEST(TestSuiteName2, TestName1) {
     ASSERT_EQ(0, 0);
+    cout << "Yaho3" << endl;
     ASSERT_NE(0, 1);
 }
 
